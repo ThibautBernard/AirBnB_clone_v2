834cd6769eaa9999d0a3b811088d2840a7e7db0b
ThibautBernard <thibautbernard@sfr.fr>
2021-03-26 13:23:38 +0000

    feat(all): add functionality to create command from the console && create tests

--- console.py
+++ console.py
@@ -10,6 +10,7 @@ from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.review import Review
+from shlex import split as sh_split
 
 
 class HBNBCommand(cmd.Cmd):
@@ -27,7 +28,11 @@ class HBNBCommand(cmd.Cmd):
     types = {
              'number_rooms': int, 'number_bathrooms': int,
              'max_guest': int, 'price_by_night': int,
-             'latitude': float, 'longitude': float
+             'latitude': float, 'longitude': float,
+             'state_id': int, 'name': str, 'city_id': str,
+             'user_id': str, 'description': str, 'text': str,
+             'email': str, 'password': str, 'first_name': str,
+             'last_name': str
             }
 
     def preloop(self):
@@ -113,18 +118,32 @@ class HBNBCommand(cmd.Cmd):
         """ Overrides the emptyline method of CMD """
         pass
 
+    def concat(self, array):
+        for z in range(len(array[1:])):
+            if (array[z][-1] == '"'):
+                array[z - 1] += " " + array[z]
+
     def do_create(self, args):
         """ Create an object of any class"""
-        if not args:
+        l_args = sh_split(args)
+        class_name = l_args[0]
+        if not class_name:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
+        elif class_name not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
+        d = {}
+        for element in l_args[1:]:
+            # seperating key and value for kwargs
+            key_v = element.split("=")
+            if HBNBCommand.types.get(key_v[0], "not found") != "not found":
+                d[key_v[0]] = HBNBCommand.\
+                 types[key_v[0]](key_v[1].replace('"', '').replace("_", ' '))
+        new_instance = HBNBCommand.classes[class_name]()
+        new_instance.__dict__.update(**d)
         storage.save()
         print(new_instance.id)
-        storage.save()
 
     def help_create(self):
         """ Help information for the create method """

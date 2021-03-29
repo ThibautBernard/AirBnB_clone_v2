ff1aaa18313ef12eaa081110ddc3551c7ef1a9fc
ThibautBernard <thibautbernard@sfr.fr>
2021-03-29 11:22:38 +0000

    fix: add nullable=

--- models/user.py
+++ models/user.py
@@ -9,8 +9,8 @@ class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
 
-    email = Column(String(128))
-    password = Column(String(128))
-    first_name = Column(String(128))
-    last_name = Column(String(128))
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
     places = relationship("Place", backref="user", cascade="all, delete")

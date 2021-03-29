44fbd7e988a51a09d603f9dad2d299964f0e4cfc
ThibautBernard <thibautbernard@sfr.fr>
2021-03-29 11:18:01 +0000

    fix: add nullable false

--- models/state.py
+++ models/state.py
@@ -12,7 +12,7 @@ class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
 
-    name = Column(String(128))
+    name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state",  cascade="all, delete")
 
     @property

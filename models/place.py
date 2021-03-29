3a06553a20fb9a5188a476c249cbba08f9bc9e45
Maynot2 <paulmanot@gmail.com>
2021-03-29 09:45:12 +0000

    Fix pep8

--- models/place.py
+++ models/place.py
@@ -12,10 +12,10 @@ class Place(BaseModel):
     user_id = Column(String(60), foreign_key("users.id"))
     name = Column(String(128), nullable=False)
     description = Column(String(1024), nullable=False)
-    number_rooms = Column (Integer(), nullable=False)
-    number_bathrooms = Column (Integer(), nullable=False)¬
-    max_guest = Column (Integer(), nullable=False)¬
-    price_by_night = Column (Integer(), nullable=False)¬
-    latitude = Column (Float(), nullable=False)¬
-    longitude = Column (Float(), nullable=False)¬
+    number_rooms = Column(Integer(), nullable=False)
+    number_bathrooms = Column(Integer(), nullable=False)¬
+    max_guest = Column(Integer(), nullable=False)¬
+    price_by_night = Column(Integer(), nullable=False)¬
+    latitude = Column(Float(), nullable=False)¬
+    longitude = Column(Float(), nullable=False)¬
     amenity_ids = []

ecc13085035a8f53c5481114868ab0c2169fcd62
ThibautBernard <thibautbernard@sfr.fr>
2021-04-14 14:11:13 +0000

    feat

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+    deploy static
+"""
+import time
+from fabric.context_managers import cd
+from fabric.api import local
+from fabric.api import get
+from fabric.api import put
+from fabric.api import reboot
+from fabric.api import run
+from fabric.api import sudo
+from fabric.api import env
+env.hosts = ['34.75.49.246', '104.196.144.160']
+
+
+def do_deploy(archive_path):
+    try:
+        put(archive_path, '/tmp/')
+        c1 = 'mkdir -p /data/web_static/releases/{}/'
+        run(c1.format(archive_path[9:-4]))
+        c2 = 'tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
+        run(c2.format(archive_path[9:], archive_path[9:-4]))
+        run('rm /tmp/{}'.format(archive_path[9:]))
+        c3 = 'mv /data/web_static/releases/{}/web_static/* \
+              /data/web_static/releases/{}/'
+        run(c3.format(archive_path[9:-4], archive_path[9:-4]))
+        c4 = 'rm -rf  /data/web_static/releases/{}/web_static/'
+        run(c4.format(archive_path[9:-4]))
+        run('rm -rf /data/web_static/current')
+        c5 = 'ln -s /data/web_static/releases/{}/ {}'
+        run(c5.format(archive_path[9:-4], '/data/web_static/current'))
+        return True
+    except:
+        return False

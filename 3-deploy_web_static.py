862a5ee35a62e9d05bcd1d558824b185673d30e0
ThibautBernard <thibautbernard@sfr.fr>
2021-04-14 18:31:09 +0000

    fix: remove do_pack outside

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -14,10 +14,10 @@ from fabric.api import env
 do_pack = __import__('1-pack_web_static').do_pack
 do_deploy = __import__('2-do_deploy_web_static').do_deploy
 env.hosts = ['34.75.49.246', '104.196.144.160']
+path = do_pack()
 
 
 def deploy():
-    path = do_pack()
     if path:
         return do_deploy(path)
     else:

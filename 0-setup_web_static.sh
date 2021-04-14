515a7909b604c81f3367cd678a066de9976b6f2e
ThibautBernard <thibautbernard@sfr.fr>
2021-04-14 10:15:58 +0000

    feat

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# some config
+sudo apt-get -y update
+sudo apt-get install nginx
+mkdir -p /data/
+mkdir -p /data/web_static/
+mkdir -p /data/web_static/releases/
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+echo "test" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+n="server_name _;\n     location \/hbnb_static { \n         alias \/data\/web_static\/current\/; \n      }"
+sed -i "s/server_name _;/$n/" /etc/nginx/sites-available/default
+sudo service nginx restart

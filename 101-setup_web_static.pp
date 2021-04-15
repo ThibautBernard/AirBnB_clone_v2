f7e6b41866e0450271772fc042ceafd9e7d4ab22
ThibautBernard <thibautbernard@sfr.fr>
2021-04-15 18:54:34 +0000

    fix

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -2,72 +2,61 @@
 
 exec { 'update':
   command => '/usr/bin/apt-get update',
-}
+} ->
 
 package { 'nginx':
-    ensure  => 'installed',
-    name    => 'nginx',
-    require => Exec['update'],
-}
+    ensure => 'installed',
+    name   => 'nginx',
+} ->
 
 file { '/data/':
-    ensure  => 'directory',
-    group   => 'ubuntu',
-    owner   => 'ubuntu',
-    require => Package['nginx'],
-}
+    ensure => 'directory',
+    group  => 'ubuntu',
+    owner  => 'ubuntu',
+} ->
 
 file { '/data/web_static/':
-    ensure  => 'directory',
-    group   => 'ubuntu',
-    owner   => 'ubuntu',
-    require => File['/data/'],
-}
+    ensure => 'directory',
+    group  => 'ubuntu',
+    owner  => 'ubuntu',
+} ->
 
 file { '/data/web_static/releases/':
-    ensure  => 'directory',
-    group   => 'ubuntu',
-    owner   => 'ubuntu',
-    require => Package['nginx'],
-}
+    ensure => 'directory',
+    group  => 'ubuntu',
+    owner  => 'ubuntu',
+} ->
 
 file { '/data/web_static/shared/':
-    ensure  => 'directory',
-    group   => 'ubuntu',
-    owner   => 'ubuntu',
-    require => Package['nginx'],
-}
+    ensure => 'directory',
+    group  => 'ubuntu',
+    owner  => 'ubuntu',
+} ->
 
 file { '/data/web_static/releases/test/':
-    ensure  => 'directory',
-    group   => 'ubuntu',
-    owner   => 'ubuntu',
-    require => Package['nginx'],
-}
+    ensure => 'directory',
+    group  => 'ubuntu',
+    owner  => 'ubuntu',
+} ->
 
 file { '/data/web_static/releases/test/index.html':
     ensure  => 'file',
     group   => 'ubuntu',
     owner   => 'ubuntu',
     content => 'test wowww',
-    require => File['/data/web_static/releases/test/'],
-}
+} ->
 
 exec { 'symbolic link':
     command  => 'ln -sf /data/web_static/releases/test/ /data/web_static/current',
     provider => 'shell',
-    require  => File['/data/web_static/releases/test/'],
-}
+} ->
 
 exec { 'sed':
   command => "sed -i \
   '/^\tlisten 80 default_server;$/i location /hbnb_static/ { alias /data/web_static/current/; }' /etc/nginx/sites-available/default",
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-  require => Package['nginx'],
-}
+} ->
 
-service { 'nginx':
-  ensure     => running,
-  hasrestart => true,
-  require    => Package['nginx'],
+exec { 'nginx restart':
+  path => '/etc/init.d/'
 }

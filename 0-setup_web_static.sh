fad38f5b716429f60562d4b863615c838de06662
ThibautBernard <thibautbernard@sfr.fr>
2021-04-15 07:35:48 +0000

    fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,12 +5,12 @@ apt-get -y update
 apt-get -y install nginx
 mkdir -p /data/
 mkdir -p /data/web_static/
-mkdir -p /data/web_static/releases
-mkdir -p /data/web_static/releases/test
-mkdir -p /data/web_static/shared
+mkdir -p /data/web_static/releases/
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
 echo "test wowww" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -hR ubuntu:ubuntu /data
 n="server_name _;\n     location \/hbnb_static\/ {\n\talias \/data\/web_static\/current\/; \n\t}"
-sed -i "s/server_name _;/$n/" /etc/nginx/sites-available/default
+sudo sed -i "s/server_name _;/$n/" /etc/nginx/sites-available/default
 service nginx restart

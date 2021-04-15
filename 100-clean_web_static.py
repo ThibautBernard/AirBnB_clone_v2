5c3661a32e62f1897fc893f60b24299c8b0290a5
ThibautBernard <thibautbernard@sfr.fr>
2021-04-15 15:49:35 +0000

    fix

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -20,8 +20,8 @@ def do_clean(number=0):
         delete all files except two most recent one, else
     """
     if number == 0 or number == 1:
-        run('ls -t /versions | tail -n +2 | xargs rm')
+        local('ls -t /versions | tail -n +2 | xargs rm')
         run('ls -t /data/web_static/releases | tail -n +2 | xargs rm')
     else:
-        run('ls -t /versions | tail -n +3 | xargs rm')
+        local('ls -t /versions | tail -n +3 | xargs rm')
         run('ls -t /data/web_static/releases | tail -n +2 | xargs rm')

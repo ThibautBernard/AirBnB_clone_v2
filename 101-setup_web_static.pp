0255fa65977783220e84a41b27a5b5bfa786cdc1
ThibautBernard <thibautbernard@sfr.fr>
2021-04-15 18:50:03 +0000

    fix

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,70 +1,73 @@
-# Confire servers to deploy
+# sets up web servers for the deployment of web_static folder
 
 exec { 'update':
   command => '/usr/bin/apt-get update',
-} ->
+}
 
 package { 'nginx':
-  ensure   => 'present',
-  provider => 'apt'
-} ->
+    ensure  => 'installed',
+    name    => 'nginx',
+    require => Exec['update'],
+}
 
 file { '/data/':
-    ensure => 'directory',
-    group  => 'ubuntu',
-    owner  => 'ubuntu',
-} ->
+    ensure  => 'directory',
+    group   => 'ubuntu',
+    owner   => 'ubuntu',
+    require => Package['nginx'],
+}
 
 file { '/data/web_static/':
-    ensure => 'directory',
-    group  => 'ubuntu',
-    owner  => 'ubuntu',
-} ->
+    ensure  => 'directory',
+    group   => 'ubuntu',
+    owner   => 'ubuntu',
+    require => File['/data/'],
+}
 
 file { '/data/web_static/releases/':
-    ensure => 'directory',
-    group  => 'ubuntu',
-    owner  => 'ubuntu',
-} ->
+    ensure  => 'directory',
+    group   => 'ubuntu',
+    owner   => 'ubuntu',
+    require => Package['nginx'],
+}
 
 file { '/data/web_static/shared/':
-    ensure => 'directory',
-    group  => 'ubuntu',
-    owner  => 'ubuntu',
-} ->
+    ensure  => 'directory',
+    group   => 'ubuntu',
+    owner   => 'ubuntu',
+    require => Package['nginx'],
+}
 
 file { '/data/web_static/releases/test/':
-    ensure => 'directory',
-    group  => 'ubuntu',
-    owner  => 'ubuntu',
-} ->
+    ensure  => 'directory',
+    group   => 'ubuntu',
+    owner   => 'ubuntu',
+    require => Package['nginx'],
+}
 
 file { '/data/web_static/releases/test/index.html':
-  ensure  => 'present',
-  path    => '/data/web_static/releases/test/index.html',
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  content => "Holberton School Puppet\n",
-} ->
+    ensure  => 'file',
+    group   => 'ubuntu',
+    owner   => 'ubuntu',
+    content => 'test wowww',
+    require => File['/data/web_static/releases/test/'],
+}
 
-file { '/data/web_static/current':
-  ensure  => 'link',
-  replace => 'yes',
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  target  => '/data/web_static/releases/test',
-} ->
-
-exec { 'chown -R ubuntu:ubuntu /data/':
-  path => '/usr/bin/:/usr/local/bin/:/bin/'
-} ->
+exec { 'symbolic link':
+    command  => 'ln -sf /data/web_static/releases/test/ /data/web_static/current',
+    provider => 'shell',
+    require  => File['/data/web_static/releases/test/'],
+}
 
 exec { 'sed':
   command => "sed -i \
   '/^\tlisten 80 default_server;$/i location /hbnb_static/ { alias /data/web_static/current/; }' /etc/nginx/sites-available/default",
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-} ->
+  require => Package['nginx'],
+}
 
-exec { 'nginx restart':
-  path => '/etc/init.d/'
+service { 'nginx':
+  ensure     => running,
+  hasrestart => true,
+  require    => Package['nginx'],
 }

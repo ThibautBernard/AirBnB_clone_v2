f60c76140d9134e69ba241a8d59c51acab335d59
ThibautBernard <thibautbernard@sfr.fr>
2021-04-15 07:32:54 +0000

    fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -11,6 +11,6 @@ mkdir -p /data/web_static/releases/test/
 echo "test" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -hR ubuntu:ubuntu /data
-n="server_name _;\n     location \/hbnb_static { \n         alias \/data\/web_static\/current\/; \n      }"
+n="server_name _;\n     location \/hbnb_static\/ {\n\talias \/data\/web_static\/current\/; \n\t}"
 sed -i "s/server_name _;/$n/" /etc/nginx/sites-available/default
 service nginx restart

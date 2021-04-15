f11d590267cb6b381ca5c15475d0190cd158b699
ThibautBernard <thibautbernard@sfr.fr>
2021-04-15 08:05:48 +0000

    fix

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -11,6 +11,7 @@ from fabric.api import reboot
 from fabric.api import run
 from fabric.api import sudo
 from fabric.api import env
+import os.path
 # do_pack = __import__('1-pack_web_static').do_pack
 # do_deploy = __import__('2-do_deploy_web_static').do_deploy
 env.hosts = ['34.75.49.246', '104.196.144.160']

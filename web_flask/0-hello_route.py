ca97a153a3d099ffc6c9b539c6fd7ffd61d767e7
ThibautBernard <thibautbernard@sfr.fr>
2021-04-29 09:56:16 +0000

    fix: doc

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+"""
+	basic route
+"""
 from flask import Flask
 app = Flask(__name__)
-#SERVER_NAME='0.0.0.0'
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     return 'Hello HBNB!'
+
 app.run(host='0.0.0.0', port=5000)

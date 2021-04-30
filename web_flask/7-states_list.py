01b2a84a8d8b7234410800c5d8cf514c94cd42c9
ThibautBernard <thibautbernard@sfr.fr>
2021-04-30 23:20:40 +0000

    fix

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -9,17 +9,17 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def teardown_db(exception):
-    """after each request"""
-    storage.close()
-
-
 @app.route('/states_list', strict_slashes=False)
 def get_all_states_route():
     """ get all states and give to the template"""
     states = storage.all(State)
     return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """after each request"""
+    storage.close()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
